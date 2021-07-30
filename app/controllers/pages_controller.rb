7b635d3696d96e35448348d7f277e44908386350
Alex Marriott <marriottaog@gmail.com>
2021-07-30 14:43:10 +0100

    maze now takes information from backend to create frontend, basic beginning of recursive function to create maze

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
-    thing = Array.new(3) { Array.new(3) { { visited?: false, walls: 'NW' } } }
-    raise
+    @grid = Array.new(3) { Array.new(3) { { visited?: false, walls: 'NW' } } }
+    create_maze(0, 0, @grid)
+    # raise
+  end
+
+  def create_maze(x_position, y_position, grid)
+    directions = ['N', 'S', 'E', 'W'].shuffle
   end
 end
