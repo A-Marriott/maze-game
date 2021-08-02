4988ea1a01ee9a5d4ee8edbd50fa269e75456796
Alex Marriott <marriottaog@gmail.com>
2021-08-02 16:46:13 +0100

    grid size is now adjustable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,29 @@
 class PagesController < ApplicationController
   def home
+    maze_size = params["q"] ? params["q"].to_i : 10
+    @css_grid = case maze_size
+                when 10
+                  'grid-size-10'
+                when 15
+                  'grid-size-15'
+                when 20
+                  'grid-size-20'
+                end
     @grid = []
     @current_recursion = 0
     @deepest_recursion = { count: 0, deepest_x_position: 0, deepest_y_position: 0 }
-    (0..9).each do |index_y|
+    (0...maze_size).each do |index_y|
       x_ary = []
-      (0..9).each do |index_x|
+      (0...maze_size).each do |index_x|
         x_ary << { x: index_x, y: index_y, visited?: false, walls: 'NW' }
       end
       @grid << x_ary
     end
     @grid[0][0][:visited?] = true
-    create_maze(0, 0, @grid)
+    create_maze(0, 0)
   end
 
-  def create_maze(y_position, x_position, grid)
+  def create_maze(y_position, x_position)
     directions = ['N', 'S', 'E', 'W'].shuffle
     @current_recursion += 1
     if @deepest_recursion[:count] < @current_recursion
@@ -27,39 +36,31 @@ class PagesController < ApplicationController
           @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('N', '')
           y_position -= 1
           @grid[y_position][x_position][:visited?] = true
-          create_maze(y_position, x_position, @grid)
+          create_maze(y_position, x_position)
         end
       when 'S'
         if (y_position + 1).between?(0, @grid.length - 1) && @grid[y_position + 1][x_position][:visited?] == false
           y_position += 1
           @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('N', '')
           @grid[y_position][x_position][:visited?] = true
-          create_maze(y_position, x_position, @grid)
+          create_maze(y_position, x_position)
         end
       when 'E'
         if (x_position + 1).between?(0, @grid.length - 1) && @grid[y_position][x_position + 1][:visited?] == false
           x_position += 1
           @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('W', '')
           @grid[y_position][x_position][:visited?] = true
-          create_maze(y_position, x_position, @grid)
+          create_maze(y_position, x_position)
         end
       when 'W'
         if (x_position - 1).between?(0, @grid.length - 1) && @grid[y_position][x_position - 1][:visited?] == false
           @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('W', '')
           x_position -= 1
           @grid[y_position][x_position][:visited?] = true
-          create_maze(y_position, x_position, @grid)
+          create_maze(y_position, x_position)
         end
       end
     end
     @current_recursion -= 1
   end
 end
-
-  # [
-  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}],
-  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}],
-  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}]
-  # ]
-
-  # ary [0, 1] == y=0, x=1
