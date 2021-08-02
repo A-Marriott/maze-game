2263d3cc0b418be8ee9e23cfbf152fa69ca68b5b
Alex Marriott <marriottaog@gmail.com>
2021-08-02 15:18:10 +0100

    added javascript to initiate completion target

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,30 +22,10 @@ ActiveStorage.start()
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import dotMovement from './init_dot_movement.js';
+import mazeTargetAndCompletion from './init_maze_target_and_completion.js';
 
 document.addEventListener('turbolinks:load', () => {
-  // maybe don't need x and y for up down
-  document.querySelector('.square').firstElementChild.className = 'player-btn';
-  let x_position = 0;
-  let y_position = 0;
-  document.addEventListener('keydown', event => {
-    if (event.key === 'ArrowDown' && document.getElementsByClassName(`y-${y_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position + 1} x-${x_position}`)[0].classList.value.includes('N')) {
-      document.querySelector('.player-btn').classList.remove('player-btn')
-      y_position += 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowUp' && document.getElementsByClassName(`y-${y_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('N')) {
-      document.querySelector('.player-btn').classList.remove('player-btn')
-      y_position -= 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowLeft' && document.getElementsByClassName(`x-${x_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('W')) {
-      document.querySelector('.player-btn').classList.remove('player-btn')
-      x_position -= 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowRight' && document.getElementsByClassName(`x-${x_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position + 1}`)[0].classList.value.includes('W')) {
-      document.querySelector('.player-btn').classList.remove('player-btn')
-      x_position += 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    }
-  })
+  dotMovement();
+  mazeTargetAndCompletion();
 });
---
+++ app/javascript/packs/init_maze_target_and_completion.js
@@ -0,0 +1,6 @@
+const mazeTargetAndCompletion = () => {
+  document.querySelector('.wrapper').children[99].children[0].className = 'target-btn';
+
+}
+
+export default mazeTargetAndCompletion;
