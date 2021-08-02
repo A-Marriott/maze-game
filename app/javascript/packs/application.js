8a462bafa71820d0bf25f4283ee9d81595e185cc
Alex Marriott <marriottaog@gmail.com>
2021-08-02 15:00:23 +0100

    successful javascript logic for movement around the map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,19 +30,19 @@ document.addEventListener('turbolinks:load', () => {
   let x_position = 0;
   let y_position = 0;
   document.addEventListener('keydown', event => {
-    if (event.key === 'ArrowDown' && document.getElementsByClassName(`y-${y_position + 1}`)[0]) {
+    if (event.key === 'ArrowDown' && document.getElementsByClassName(`y-${y_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position + 1} x-${x_position}`)[0].classList.value.includes('N')) {
       document.querySelector('.player-btn').classList.remove('player-btn')
       y_position += 1
       document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowUp' && document.getElementsByClassName(`y-${y_position - 1}`)[0]) {
+    } else if (event.key === 'ArrowUp' && document.getElementsByClassName(`y-${y_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('N')) {
       document.querySelector('.player-btn').classList.remove('player-btn')
       y_position -= 1
       document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowLeft' && document.getElementsByClassName(`x-${x_position - 1}`)[0]) {
+    } else if (event.key === 'ArrowLeft' && document.getElementsByClassName(`x-${x_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('W')) {
       document.querySelector('.player-btn').classList.remove('player-btn')
       x_position -= 1
       document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
-    } else if (event.key === 'ArrowRight' && document.getElementsByClassName(`x-${x_position + 1}`)[0]) {
+    } else if (event.key === 'ArrowRight' && document.getElementsByClassName(`x-${x_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position + 1}`)[0].classList.value.includes('W')) {
       document.querySelector('.player-btn').classList.remove('player-btn')
       x_position += 1
       document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.className = 'player-btn';
