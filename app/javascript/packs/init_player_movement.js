b4a6aaf567a5236f904e90b6df25eb07478dcd5b
Alex Marriott <marriottaog@gmail.com>
2021-08-02 18:25:48 +0100

    refactored player movement js code

--- app/javascript/packs/init_player_movement.js
+++ app/javascript/packs/init_player_movement.js
@@ -2,30 +2,36 @@ import timer from './init_timer.js'
 
 const playerMovement = () => {
   document.querySelector('.square').firstElementChild.className = 'player';
-  let x_position = 0;
-  let y_position = 0;
+  let xPosition = 0;
+  let yPosition = 0;
   let timerStarted = false;
+  const xMovement = {'ArrowLeft': -1, 'ArrowRight': 1};
+  const yMovement = {'ArrowUp': -1, 'ArrowDown': 1};
+
+  const movePlayer = (keyPress) => {
+    document.querySelector('.player').classList.remove('player')
+    if (xMovement[keyPress]) {
+      xPosition += xMovement[keyPress]
+    } else {
+      yPosition += yMovement[keyPress]
+    }
+    document.getElementsByClassName(`x-${xPosition} y-${yPosition}`)[0].firstElementChild.classList.add('player');
+  }
+
   document.addEventListener('keydown', event => {
     if (timerStarted === false) {
       timer();
       timerStarted = true;
     }
-    if (event.key === 'ArrowDown' && document.getElementsByClassName(`y-${y_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position + 1} x-${x_position}`)[0].classList.value.includes('N')) {
-      document.querySelector('.player').classList.remove('player')
-      y_position += 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.classList.add('player');
-    } else if (event.key === 'ArrowUp' && document.getElementsByClassName(`y-${y_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('N')) {
-      document.querySelector('.player').classList.remove('player')
-      y_position -= 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.classList.add('player');
-    } else if (event.key === 'ArrowLeft' && document.getElementsByClassName(`x-${x_position - 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position}`)[0].classList.value.includes('W')) {
-      document.querySelector('.player').classList.remove('player')
-      x_position -= 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.classList.add('player');
-    } else if (event.key === 'ArrowRight' && document.getElementsByClassName(`x-${x_position + 1}`)[0] && !document.getElementsByClassName(`y-${y_position} x-${x_position + 1}`)[0].classList.value.includes('W')) {
-      document.querySelector('.player').classList.remove('player')
-      x_position += 1
-      document.getElementsByClassName(`x-${x_position} y-${y_position}`)[0].firstElementChild.classList.add('player');
+
+    if (event.key === 'ArrowDown' && document.getElementsByClassName(`y-${yPosition + 1}`)[0] && !document.getElementsByClassName(`y-${yPosition + 1} x-${xPosition}`)[0].classList.value.includes('N')) {
+      movePlayer(event.key)
+    } else if (event.key === 'ArrowUp' && document.getElementsByClassName(`y-${yPosition - 1}`)[0] && !document.getElementsByClassName(`y-${yPosition} x-${xPosition}`)[0].classList.value.includes('N')) {
+      movePlayer(event.key)
+    } else if (event.key === 'ArrowLeft' && document.getElementsByClassName(`x-${xPosition - 1}`)[0] && !document.getElementsByClassName(`y-${yPosition} x-${xPosition}`)[0].classList.value.includes('W')) {
+      movePlayer(event.key)
+    } else if (event.key === 'ArrowRight' && document.getElementsByClassName(`x-${xPosition + 1}`)[0] && !document.getElementsByClassName(`y-${yPosition} x-${xPosition + 1}`)[0].classList.value.includes('W')) {
+      movePlayer(event.key)
     }
   })
 }
