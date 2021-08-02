ebd29cd3f594e312d245905e83ce946be46b6302
Alex Marriott <marriottaog@gmail.com>
2021-08-02 13:32:23 +0100

    backend to maze generation complete, needs heavy refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,52 @@
 class PagesController < ApplicationController
   def home
-    @grid = Array.new(3) { Array.new(3) { { visited?: false, walls: 'NW' } } }
+    @grid = Array.new(10) { Array.new(10) { { visited?: false, walls: 'NW' } } }
+    @grid[0][0][:visited?] = true
     create_maze(0, 0, @grid)
-    # raise
   end
 
-  def create_maze(x_position, y_position, grid)
+  def create_maze(y_position, x_position, grid)
     directions = ['N', 'S', 'E', 'W'].shuffle
 
     directions.each do |direction|
-
+      case direction
+      when 'N'
+        if (y_position - 1).between?(0, @grid.length - 1) && @grid[y_position - 1][x_position][:visited?] == false
+          @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('N', '')
+          y_position -= 1
+          @grid[y_position][x_position][:visited?] = true
+          create_maze(y_position, x_position, @grid)
+        end
+      when 'S'
+        if (y_position + 1).between?(0, @grid.length - 1) && @grid[y_position + 1][x_position][:visited?] == false
+          y_position += 1
+          @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('N', '')
+          @grid[y_position][x_position][:visited?] = true
+          create_maze(y_position, x_position, @grid)
+        end
+      when 'E'
+        if (x_position + 1).between?(0, @grid.length - 1) && @grid[y_position][x_position + 1][:visited?] == false
+          x_position += 1
+          @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('W', '')
+          @grid[y_position][x_position][:visited?] = true
+          create_maze(y_position, x_position, @grid)
+        end
+      when 'W'
+        if (x_position - 1).between?(0, @grid.length - 1) && @grid[y_position][x_position - 1][:visited?] == false
+          @grid[y_position][x_position][:walls] = @grid[y_position][x_position][:walls].tr('W', '')
+          x_position -= 1
+          @grid[y_position][x_position][:visited?] = true
+          create_maze(y_position, x_position, @grid)
+        end
+      end
     end
   end
 end
+
+  # [
+  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}],
+  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}],
+  # [{:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}, {:visited?=>false, :walls=>"NW"}]
+  # ]
+
+  # ary [0, 1] == y=0, x=1
