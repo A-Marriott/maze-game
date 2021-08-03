8d4ce2a7c0b90dbc30774a9e287acc20237ba64c
Alex Marriott <marriottaog@gmail.com>
2021-08-03 13:56:33 +0100

    updated completion check text

--- app/javascript/packs/init_maze_completion_check.js
+++ app/javascript/packs/init_maze_completion_check.js
@@ -2,7 +2,7 @@ const mazeCompletionCheck = () => {
   document.addEventListener('keydown', () => {
     if (document.getElementsByClassName('player target')[0]) {
       setTimeout(() => {
-        alert(`You won! It took you ${document.getElementById('second-count').innerHTML} to complete`);
+        alert(`You won in ${document.getElementById('second-count').innerHTML}!`);
         location.reload();
       }, 1);
     }
